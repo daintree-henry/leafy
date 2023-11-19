d51febccbb182ac1c92336ccc89f29b41df703e4
henry <daintree.henry@gmail.com>
2023-11-19 21:24:38 +0900

    #스크립트 업데이트

--- leafy-backend/script.sh
+++ leafy-backend/script.sh
@@ -1,9 +1,11 @@
-#도커 이미지 빌드 및 푸시
-docker build -t {TYPE_YOUR_REPOSITORY}/leafy-backend:1.0.0 . --no-cache
-docker push {TYPE_YOUR_REPOSITORY}/leafy-backend:1.0.0 
+#1. 이미지 빌드 및 푸시 easydocker/leafy/leafy-backend 경로에서 실행
+docker build -t (개인레지스트리명)/leafy-backend:1.0.0 . --no-cache
+docker push (개인레지스트리명)/leafy-backend:1.0.0 
 
-# 컨테이너 생성 (윈도우 cmd or Powershell 사용)
-docker run -d -p 8080:8080 -e DB_URL=leafy-postgres --network leafy-network --name leafy {TYPE_YOUR_REPOSITORY}/leafy:1.0.0
+#2. 컨테이너 실행 및 로그 확인
+docker run -d -p 8080:8080 -e DB_URL=leafy-postgres --name leafy --network leafy-network (개인레지스트리명)/leafy-backend:1.0.0 
+docker logs leafy-backend
 
-# 로그 확인
-docker logs -f leafy
+#3. API 테스트
+curl http://localhost:8080/api/v1/users
+curl http://localhost:8080/api/v1/plant-logs/1
--- leafy-frontend/script.sh
+++ leafy-frontend/script.sh
@@ -1,9 +1,7 @@
-#도커 이미지 빌드 및 푸시
-docker build -t {TYPE_YOUR_REPOSITORY}/leafy-front:1.0.0 . --no-cache
-docker push {TYPE_YOUR_REPOSITORY}/leafy-front:1.0.0
+# 1. 이미지 빌드 및 푸시 (easydocker/part5/leafy 경로에서 실행)
+docker build -t (개인레지스트리명)/leafy-frontend:1.0.0 . --no-cache
+docker push (개인레지스트리명)/leafy-frontend:1.0.0 
 
-# 컨테이너 생성 (윈도우 cmd or Powershell 사용)
-docker run -d -p 80:80 --network leafy-network --name leafy-front {TYPE_YOUR_REPOSITORY}/leafy-front:1.0.0
-
-# 로그 확인 및 http://localhost 접속
+# 2. 컨테이너 실행 및 로그 확인, localhost 접속 테스트
+docker run -d -p 80:80 --name leafy-front --network leafy-network (개인레지스트리명)/leafy-frontend:1.0.0 
 docker logs -f leafy-front
